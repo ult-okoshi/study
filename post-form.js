83dc329db808de9379706bd25d059fd45a5ef71a
Okoshi Miu <m.okoshi@ultinet.co.jp>
2025-03-11 16:09:04 +0900

    投稿処理のif文を削除

--- post-form.js
+++ post-form.js
@@ -11,24 +11,19 @@ class PostForm {
   static postSave(){
     const postContent = $('#post-content').val();
     const postList = PostRepository.getPosts();
-    let errorCount = 0;
     if (postContent.trim() === '') {
       ErrorMessageShow.spaceError();
-      errorCount = 1;
       $('#post-content').val('');
       return false;
     }
     if (postList.length >= 50) {
       ErrorMessageShow.limitError();
-      errorCount = 1;
       $('#post-content').val('');
       return false;
     }
-    if (errorCount === 0) {
       ErrorMessageShow.noError();
       PostRepository.create(postContent);
       $('#post-content').val('');
-    }
   }
 }
 
